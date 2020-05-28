685d8e9a25302f3995f81c3419dc6079723987e3
novadevco <novadevco@gmail.com>
2020-05-28 00:46:13 -0500

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { EditorModule } from '@progress/kendo-angular-editor';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { MentionDirective } from './mention.directive';
-
+import 'hammerjs';
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule,EditorModule ],
   declarations: [ AppComponent, HelloComponent, MentionDirective ],
   bootstrap:    [ AppComponent ]
 })
